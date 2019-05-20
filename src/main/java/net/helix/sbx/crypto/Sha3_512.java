decc04550a4f6e95839d5da27db20abcb36ab0d8
spangin <>
2019-05-20 18:05:18 +0300

    sha3-512 was added

---
+++ src/main/java/net/helix/sbx/crypto/Sha3_512.java
@@ -0,0 +1,58 @@
+package net.helix.sbx.crypto;
+
+import org.bouncycastle.jcajce.provider.digest.SHA3;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.security.DigestException;
+
+// Sha3-512
+
+public class Sha3_512 implements Sponge {
+
+    private static final Logger log = LoggerFactory.getLogger(Sha3_512.class);
+    public static final int HASH_LENGTH = 64;
+
+    private final SHA3.Digest512 sha;
+
+    protected Sha3_512() {
+        this.sha = new SHA3.Digest512();
+    }
+
+    @Override
+    public void absorb(final byte[] bytes, final int offset, final int length) {
+        if (bytes.length < length) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (length % HASH_LENGTH != 0) {
+            throw new RuntimeException("Illegal length: " + length);
+        }
+        for (int pos = offset; pos < offset + length; pos += HASH_LENGTH) {
+            sha.update(bytes, pos, HASH_LENGTH);
+        }
+    }
+    @Override
+    public void squeeze(final byte[] bytes, final int offset, final int length) {
+        if (bytes.length < length) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (length % HASH_LENGTH != 0) {
+            throw new RuntimeException("Illegal length: " + length);
+        }
+        try {
+            for (int pos = offset; pos < offset + length; pos += HASH_LENGTH) {
+                sha.digest(bytes, pos, HASH_LENGTH);
+                sha.update(bytes, pos, HASH_LENGTH);
+            }
+
+        } catch (DigestException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void reset() {
+        this.sha.reset();
+    }
+
+}
--- src/main/java/net/helix/sbx/crypto/SpongeFactory.java
+++ src/main/java/net/helix/sbx/crypto/SpongeFactory.java
@@ -7,14 +7,15 @@ public abstract class SpongeFactory {
     public enum Mode {
         K256,
         K512,
-        S256
+        S256,
+        S512
     }
     public static Sponge create(Mode mode){
         switch (mode) {
             case K256: return new K256();
             case K512: return new K512();
             case S256: return new Sha3();
-
+            case S512: return new Sha3_512();
             default: return null;
         }
     }
