8bc4373ab03f85b0e7b0d5679be703ba4e0b4eac
fsbbn <f.sophie.abben@gmail.com>
2019-06-12 13:47:09 +0200

    add spammer

---
+++ src/main/java/net/helix/hlx/service/Spammer.java
@@ -0,0 +1,52 @@
+package net.helix.hlx.service;
+
+import net.helix.hlx.model.Hash;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class Spammer {
+    private static final Logger log = LoggerFactory.getLogger(Spammer.class);
+    private final ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+    private API api;
+
+    private String address;
+    private String message;
+    private int delay;
+
+    public Spammer(API api) {
+        this.api = api;
+        this.message = StringUtils.repeat('0', 1024*2);
+        this.address = Hash.NULL_HASH.hexString();
+        this.delay = 50;
+    }
+
+    public void startScheduledExecutorService() {
+        log.info("Spammer scheduledExecutorService started.");
+        log.info("Submitting Tx every: " + this.delay + "ms.");
+        this.scheduledExecutorService.scheduleWithFixedDelay(this.getRunnableSendTx(), 10000, this.delay,  TimeUnit.MILLISECONDS);
+    }
+
+    private void sendTx() throws Exception {
+        this.api.attachStoreAndBroadcast(this.address, this.message);
+    }
+
+    private Runnable getRunnableSendTx() {
+        return () -> {
+            try {
+                sendTx();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
+    }
+
+    public void shutdown() {
+        log.info("Shutting down Spammer Thread");
+        scheduledExecutorService.shutdown();
+    }
+}
