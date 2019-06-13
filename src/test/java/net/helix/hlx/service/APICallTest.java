780a6953bc4e28fe731aee80ef4ef728937e3fb2
spangin <>
2019-06-13 11:37:14 +0300

    test for APICall

---
+++ src/test/java/net/helix/hlx/service/APICallTest.java
@@ -0,0 +1,31 @@
+package net.helix.hlx.service;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import net.helix.hlx.conf.HelixConfig;
+
+
+public class APICallTest {
+    
+    private API api;
+    
+    @Before
+    public void setup() {
+        HelixConfig configuration = Mockito.mock(HelixConfig.class);
+        api = new API(configuration, null, null, null, null, null, null, null,
+                null, null, null, null, null, null);
+    }
+
+    @Test
+    public void apiHasAllEnums() {
+        for (ApiCommand c : ApiCommand.values()) {
+            if (!api.commandRoute.containsKey(c)) {
+                Assert.fail("Api should contain all enum values");
+            }
+        }
+    }
+
+}
