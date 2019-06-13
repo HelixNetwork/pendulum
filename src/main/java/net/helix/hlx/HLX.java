3cd88486c40a85cb36d989e228fc1583a50bf215
ofo42 <of@hlx.ai>
2019-06-13 17:15:12 +0200

    bump version

--- src/main/java/net/helix/hlx/HLX.java
+++ src/main/java/net/helix/hlx/HLX.java
@@ -46,7 +46,7 @@ public class HLX {
 
     public static final String MAINNET_NAME = "HLX";
     public static final String TESTNET_NAME = "HLX Testnet";
-    public static final String VERSION = "0.5.4";
+    public static final String VERSION = "0.5.5";
 
     /**
      * The entry point of the helix sandbox.
