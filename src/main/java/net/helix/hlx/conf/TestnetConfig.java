0cb33482ef2c27a9fe69c2e731b37f4987cb3a12
ofo42 <of@hlx.ai>
2019-06-17 23:25:52 +0200

    Change defaults for testnet config

--- src/main/java/net/helix/hlx/conf/TestnetConfig.java
+++ src/main/java/net/helix/hlx/conf/TestnetConfig.java
@@ -170,7 +170,7 @@ public class TestnetConfig extends BaseHelixConfig {
         int MWM = 1;
         int MILESTONE_START_INDEX = 0;
         int KEYS_IN_MILESTONE = 10;
-        int PACKET_SIZE = 1200;
+        int PACKET_SIZE = 800;
         String DB_PATH = "testnetdb";
         String DB_LOG_PATH = "testnetdb.log";
     }
