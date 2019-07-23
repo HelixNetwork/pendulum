c2b93c97c5cf6ae55741fa874db8e8d52d84b0d5
fsbbn <f.sophie.abben@gmail.com>
2019-07-23 14:22:22 +0200

    genesis time

--- src/main/java/net/helix/hlx/conf/BaseHelixConfig.java
+++ src/main/java/net/helix/hlx/conf/BaseHelixConfig.java
@@ -899,7 +899,7 @@ public abstract class BaseHelixConfig implements HelixConfig {
         String COORDINATOR_ADDRESS = "2bebfaee978c03e3263c3e5480b602fb040a120768c41d8bfae6c0c124b8e82a";
         int MS_DELAY = 0;
         int MS_MIN_DELAY = 5;
-        long GENESIS_TIME = 1563285823554L;
+        long GENESIS_TIME = 1563883196073L;
         int ROUND_DURATION = 5000;
 
         //Snapshot
