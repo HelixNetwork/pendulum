892dcf1b93ed68083075b7f45ab5dd786b72ea09
fsbbn <f.sophie.abben@gmail.com>
2019-07-30 17:09:22 +0200

    implement NomineePublisher

---
+++ src/main/java/net/helix/hlx/service/curator/impl/NomineePublisher.java
@@ -0,0 +1,61 @@
+package net.helix.hlx.service.curator.impl;
+
+import net.helix.hlx.conf.HelixConfig;
+import net.helix.hlx.service.API;
+import net.helix.hlx.service.milestone.MilestonePublisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class NomineePublisher {
+    private static final Logger log = LoggerFactory.getLogger(MilestonePublisher.class);
+    private final ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+    private HelixConfig config;
+    private API api;
+
+    private int delay;
+    private int mwm;
+    private Boolean sign;
+    private int currentKeyIndex;
+    private int startRoundDelay;
+
+    public NomineePublisher(HelixConfig configuration, API api) {
+        this.config = configuration;
+        this.api = api;
+        this.delay = 30000;
+        this.mwm = 2;
+        this.sign = !this.config.isDontValidateTestnetMilestoneSig();
+        this.currentKeyIndex = 0;
+        this.startRoundDelay = 2;
+    }
+
+    public void startScheduledExecutorService() {
+        log.info("NomineePublisher scheduledExecutorService started.");
+        log.info("Update Nominees every: " + delay / 1000 + "s.");
+        this.scheduledExecutorService.scheduleWithFixedDelay(this.getRunnableUpdateNominees(), this.delay, this.delay,  TimeUnit.MILLISECONDS);
+    }
+
+    private void UpdateNominees() throws Exception {
+        log.info("Publishing new Nominees...");
+        this.api.updateNominees(this.startRoundDelay, this.mwm, this.sign, this.currentKeyIndex);
+        this.currentKeyIndex += 1;
+    }
+
+    private Runnable getRunnableUpdateNominees() {
+        return () -> {
+            try {
+                UpdateNominees();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
+    }
+
+    public void shutdown() {
+        log.info("Shutting down NomineePublisher Thread");
+        scheduledExecutorService.shutdown();
+    }
+}
