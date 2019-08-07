e8604073687325eb303396dfcc59736e235a0e53
spangin <>
2019-08-07 14:55:22 +0300

    default constructor for Hash-classes (deserialization support)

--- src/main/java/net/helix/hlx/model/BundleNonceHash.java
+++ src/main/java/net/helix/hlx/model/BundleNonceHash.java
@@ -1,6 +1,10 @@
 package net.helix.hlx.model;
 
 public class BundleNonceHash extends AbstractHash {
+    
+    public BundleNonceHash() {
+    }
+    
     protected BundleNonceHash(byte[] tagBytes, int offset, int tagSizeInBytes) {
         super(tagBytes, offset, tagSizeInBytes);
     }
