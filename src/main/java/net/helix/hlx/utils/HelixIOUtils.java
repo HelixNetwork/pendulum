e6ecec531bf8fb85385b492c78eed3a4276e90de
dnck <dnck@users.noreply.github.com>
2019-09-05 11:13:26 +0200

    save logs refactor

--- src/main/java/net/helix/hlx/utils/HelixIOUtils.java
+++ src/main/java/net/helix/hlx/utils/HelixIOUtils.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.UUID;
 
 public class HelixIOUtils extends IOUtils {
 
@@ -22,21 +23,23 @@ public class HelixIOUtils extends IOUtils {
     }
 
     public static void saveLogs() {
-        String date_parsed = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(new Date());
-        String root_dir = System.getProperty("user.dir");
-        String slash = System.getProperty("file.separator");
-        String logback_xml_filepath = root_dir + slash + "src" + slash + "main" + slash + "resources" + slash + "logback-save.xml";
-        String logs_dir = root_dir + slash + "logs";
-        String log_filepath = root_dir + slash + "logs" + slash + "LOG__"+date_parsed+"__.log";
-
-        System.setProperty("log.name", log_filepath);
-        System.setProperty("logback.configurationFile", logback_xml_filepath);
-
-        File path_to_log_dir = Paths.get(logs_dir).toFile();
-
+        String dateParsed = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(new Date());
+        UUID uuid = UUID.randomUUID();
+        String uuidString = uuid.toString();
+        String rootDir = System.getProperty("user.dir");
+        String fileSeperator = System.getProperty("file.separator");
+        String logbackXmlFilepath = String.join(
+                fileSeperator, rootDir, "src", "main", "resources", "logback-save.xml"
+        );
+        String logsDir = String.join(fileSeperator, rootDir, "logs");
+        String logName = "log-" + dateParsed + "-" + uuidString + ".log";
+        String logFilepath =   String.join(fileSeperator, rootDir, "logs", logName);
+        System.setProperty("log.name", logFilepath);
+        System.setProperty("logback.configurationFile", logbackXmlFilepath);
+        File pathToLogDir = Paths.get(logsDir).toFile();
         // check whether path to logs exists and logs is a directory.
-        if (!path_to_log_dir.exists() || !path_to_log_dir.isDirectory()) {
-            path_to_log_dir.mkdir();
+        if (!pathToLogDir.exists() || !pathToLogDir.isDirectory()) {
+            pathToLogDir.mkdir();
         }
     }
 }
