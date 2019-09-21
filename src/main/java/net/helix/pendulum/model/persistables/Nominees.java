86a6d26bfc4d6dec17cd96d52e6d316fb35d6aca
fsbbn <f.sophie.abben@gmail.com>
2019-09-21 17:03:04 +0200

    add additional files

---
+++ src/main/java/net/helix/pendulum/model/persistables/Nominees.java
@@ -0,0 +1,22 @@
+package net.helix.pendulum.model.persistables;
+
+import net.helix.pendulum.model.IntegerIndex;
+import net.helix.pendulum.utils.Serializer;
+import org.apache.commons.lang3.ArrayUtils;
+
+public class Nominees extends Hashes {
+    public IntegerIndex index;
+
+    @Override
+    public byte[] bytes() {
+        return ArrayUtils.addAll(index.bytes(), super.bytes());
+    }
+
+    @Override
+    public void read(byte[] bytes) {
+        if (bytes != null) {
+            index = new IntegerIndex(Serializer.getInteger(bytes));
+            read(bytes, Integer.BYTES);
+        }
+    }
+}
