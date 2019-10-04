bb9d07a4b936b77be30fcbdace796c8d86b0e494
ofo42 <of@hlx.ai>
2019-10-04 13:23:51 +0200

    Set to logging-level trace

--- src/main/java/net/helix/pendulum/service/milestone/impl/MilestoneServiceImpl.java
+++ src/main/java/net/helix/pendulum/service/milestone/impl/MilestoneServiceImpl.java
@@ -184,7 +184,7 @@ public class MilestoneServiceImpl implements MilestoneService {
 
                             Hash senderAddress = tail.getAddressHash();
                             boolean validSignature = Merkle.validateMerkleSignature(bundleTransactionViewModels, mode, senderAddress, securityLevel, config.getMilestoneKeyDepth());
-
+                            log.trace("valid signature: {}", validSignature);
                             if ((config.isTestnet() && config.isDontValidateTestnetMilestoneSig()) ||
                                     (validatorAddresses.contains(senderAddress)) && validSignature) {
 
