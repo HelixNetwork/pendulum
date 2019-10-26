fa8913f2ddd821ca6ff13874db1b19138ded60bc
ofo42 <of@hlx.ai>
2019-10-26 14:01:34 +0200

    cleanup

--- src/main/java/net/helix/pendulum/service/ledger/impl/LedgerServiceImpl.java
+++ src/main/java/net/helix/pendulum/service/ledger/impl/LedgerServiceImpl.java
@@ -80,12 +80,6 @@ public class LedgerServiceImpl implements LedgerService {
         return this;
     }
 
-
-    @Override
-    public boolean updateDiff(Set<Hash> approvedHashes, final Map<Hash, Long> diff, Hash tip) {
-        return true;
-    }
-
     @Override
     public void restoreLedgerState() throws LedgerException {
         try {
@@ -129,7 +123,6 @@ public class LedgerServiceImpl implements LedgerService {
 
         return true;
     }
-    // TODO: remove debug verbosity
     @Override
     public boolean isBalanceDiffConsistent(Set<Hash> approvedHashes, Map<Hash, Long> diff, Hash tip) throws
             LedgerException {
@@ -158,8 +151,7 @@ public class LedgerServiceImpl implements LedgerService {
                 currentState.putIfAbsent(key, value);
             }
         });
-        boolean isConsistent = snapshotProvider.getLatestSnapshot().patchedState(new SnapshotStateDiffImpl(
-                currentState)).isConsistent();
+        boolean isConsistent = snapshotProvider.getLatestSnapshot().patchedState(new SnapshotStateDiffImpl(currentState)).isConsistent();
         if (isConsistent) {
             diff.putAll(currentState);
             approvedHashes.addAll(visitedHashes);
