a6b3efc878c95410d987993118e399371ad96522
ofo42 <of@hlx.ai>
2019-10-26 15:00:38 +0200

    Fix: getBalances is now always based on latestSnapshot. NPE was caused by not passing references and rvm.get returning null on 0-index. References/tips parameter is not relevant for our implementation.

--- src/main/java/net/helix/pendulum/service/dto/GetBalancesResponse.java
+++ src/main/java/net/helix/pendulum/service/dto/GetBalancesResponse.java
@@ -18,11 +18,6 @@ public class GetBalancesResponse extends AbstractResponse {
 	private List<String> balances;
 
 	/**
-	 * The tips used to view the balances. If none were supplied this will be the latest confirmed milestone.
-	 */
-	private List<String> references;
-
-	/**
 	 * The milestone index with which the confirmed balance was determined
 	 */
 	private int milestoneIndex;
@@ -31,24 +26,14 @@ public class GetBalancesResponse extends AbstractResponse {
 	 * Creates a new {@link GetBalancesResponse}
 	 *
 	 * @param elements {@link #balances}
-	 * @param references {@link #references}
 	 * @param milestoneIndex {@link #milestoneIndex}
 	 * @return an {@link GetBalancesResponse} filled with the balances, references used and index used
 	 */
-	public static AbstractResponse create(List<String> elements, List<String> references, int milestoneIndex) {
+	public static AbstractResponse create(List<String> elements,  int milestoneIndex) {
 		GetBalancesResponse res = new GetBalancesResponse();
 		res.balances = elements;
-		res.references = references;
 		res.milestoneIndex = milestoneIndex;
 		return res;
-	}
-
-	/**
-	 *
-	 * @return {@link #references}
-	 */
-	public List<String> getReferences() {
-		return references;
 	}
 
 	/**
