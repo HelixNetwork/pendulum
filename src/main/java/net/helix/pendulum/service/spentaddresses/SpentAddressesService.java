71f93ce56fa88328f300e275b7fe7997ed6ada32
ofo42 <of@hlx.ai>
2019-10-27 12:51:51 +0100

    Refactor persistSpentAddresses

--- src/main/java/net/helix/pendulum/service/spentaddresses/SpentAddressesService.java
+++ src/main/java/net/helix/pendulum/service/spentaddresses/SpentAddressesService.java
@@ -1,5 +1,6 @@
 package net.helix.pendulum.service.spentaddresses;
 
+import net.helix.pendulum.conf.PendulumConfig;
 import net.helix.pendulum.controllers.TransactionViewModel;
 import net.helix.pendulum.model.Hash;
 
@@ -19,15 +20,6 @@ public interface SpentAddressesService {
      * @throws SpentAddressesException
      */
     boolean wasAddressSpentFrom(Hash addressHash) throws SpentAddressesException;
-
-    /**
-     * Calculates and persists all spent addresses in between a range that were validly signed
-     *
-     * @param fromMilestoneIndex the lower bound milestone index (inclusive)
-     * @param toMilestoneIndex the upper bound milestone index (exclusive)
-     * @throws Exception when anything went wrong whilst calculating.
-     */
-    void persistSpentAddresses(int fromMilestoneIndex, int toMilestoneIndex) throws Exception;
 
     /**
      * Persist all the verifiable spent from a given list of transactions
