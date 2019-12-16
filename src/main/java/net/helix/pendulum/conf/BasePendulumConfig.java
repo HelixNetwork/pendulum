da23d9bc75aafb8094369f6ad6b3cc701de49d7f
ofo42 <of@hlx.ai>
2019-12-16 16:34:45 +0100

    Increase round duration and update genesis time

--- src/main/java/net/helix/pendulum/conf/BasePendulumConfig.java
+++ src/main/java/net/helix/pendulum/conf/BasePendulumConfig.java
@@ -1036,14 +1036,12 @@ public abstract class BasePendulumConfig implements PendulumConfig {
                 HashFactory.ADDRESS.create("a5afe01e64ae959f266b382bb5927fd07b49e7e3180239535126844aaae9bf93"),
                 HashFactory.ADDRESS.create("e2debe246b5d1a6e05b57b0fc14edb51d136966a91a803b523586ad032f72f3d"),
                 HashFactory.ADDRESS.create("1895a039c85b9a5c4e822c8fc51884aedecddfa09daccef642fff697157657b4"),
-                HashFactory.ADDRESS.create("1895a039c85b9a5c4e822c8fc51884aedecddfa09daccef642fff697157657b4"),
-                HashFactory.ADDRESS.create("1c6b0ee311a7ddccf255c1097995714b285cb06628be1cef2080b0bef7700e12"),
-                HashFactory.ADDRESS.create("eb0d925c1cfa4067db65e4b93fa17d451120cc5a719d637d44a39a983407d832")
+                HashFactory.ADDRESS.create("1c6b0ee311a7ddccf255c1097995714b285cb06628be1cef2080b0bef7700e12")
         ));
 
-        long GENESIS_TIME = 1569024001000L;
-        int ROUND_DURATION = 15000;
-        int ROUND_PAUSE = 5000;
+        long GENESIS_TIME = 1576510288107L;
+        int ROUND_DURATION = 45000;
+        int ROUND_PAUSE = 15000;
         String VALIDATOR_KEYFILE = "/Validator.key";
         String VALIDATOR_SEED_PATH = "/Validator.txt";
         int MILESTONE_KEY_DEPTH = 10;
